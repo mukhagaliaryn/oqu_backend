741ab09c88ada0ea8d6709b0898b0fd5d2a81fc7
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-09-19 13:04:53 +0600

    Done

--- core/settings.py
+++ core/settings.py
@@ -34,14 +34,9 @@ INSTALLED_APPS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    "https://www.mediahosting.kz",
-    "https://oquplatforms.vercel.app",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-]
-CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = ['*']
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 SILENCED_SYSTEM_CHECKS = ['security.W019']
 
