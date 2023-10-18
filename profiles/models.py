44c7d695bb8a00dab3251ee721927712ea2a0b50
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-10-18 10:39:22 +0600

    Done

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from accounts.models import User
-from products.models import Task, Question, Answer, Chapter, Lesson, Video
+from products.models import Product, Task, Question, Answer, Chapter, Lesson, Video
 
 
 # Profile model
 # -----------------------------------------------------------------------------------------------
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    products = models.ManyToManyField(to='products.Product', verbose_name='Продукты', blank=True)
+    products = models.ManyToManyField(Product, verbose_name='Продукты', blank=True)
 
     def __str__(self):
         return f'Профиль: {self.user}'
