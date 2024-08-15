881b6399ad1a0f127690271194e29ea935a0ef4b
mukhagaliaryn <mukhagaliaryn@gmail.com>
2024-08-15 17:40:48 +0500

    Done

--- core/settings.py
+++ core/settings.py
@@ -153,11 +153,16 @@ else:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_DEFAULT_ACL = 'public-read'
-
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-    DEFAULT_FILE_STORAGE = 'core.storages.MediaStore'
+    STORAGES = {
+        'default': {
+            'BACKEND': 'core.storages.MediaStore',
+        },
+        'staticfiles': {
+            'BACKEND': 'storages.backends.s3boto3.S3StaticStorage',
+        },
+    }
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
